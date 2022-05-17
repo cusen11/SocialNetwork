99b3518aba26e3831189e75de8674e9d080aa398
cusen11 <you@example.com>
2022-05-17 10:12:51 +0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path')
 
 const app = express();
 //connect DB
@@ -10,9 +10,6 @@ connectDB();
 
 app.use(express.json({ extended: false })); 
 
-app.get('/',(req,res)=>{
-    res.send('Hello Word!!')
-})
 
 //Define Route
 
@@ -21,6 +18,15 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+// server static assets in production 
+
+if(process.env.NODE_ENV === 'production'){
+    // set statuc folder
+    app.use(express.static('client/build'))
+    app.get('*',(req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server start on PORT ${PORT}`))
