a10f9b2920f72986ed4b0d488224bd310dc06124
cusen11 <cusenbonchen@gmail.com>
2022-07-13 22:50:22 +0700

    make it better

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const server = app.listen(PORT, ()=> console.log(`Server start on PORT ${PORT}`)
 const io = require('socket.io')(server,{
     pingTimeout: 60000,
     cors:{
-        origin: 'http://localhost:3000' 
+        origin: 'https://senbook.herokuapp.com/' 
     }
 })
 let users =[]
